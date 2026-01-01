189e58bfd312a402ba3a43be665b7f4b996b7d19
zumepreal <zumepreal@gmail.com>
2026-01-01 17:34:24 +0300

    demo2

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-const CACHE_NAME = 'flex-v1';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'tasker-v2';
+const ASSETS = [
+  'index.html',
+  'via.placeholder.com',
+  'via.placeholder.com'
+];
 
-// Кешируем файлы при установке
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-// Работаем из кеша, если нет сети
+// Стратегия: Сначала кеш, если нет - сеть
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
